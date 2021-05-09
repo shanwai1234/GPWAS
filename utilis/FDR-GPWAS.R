92c212fae771b65d5a3087fa69cd59675b3d8654
Zhikai <zliang@huskers.unl.edu>
2021-05-08 21:38:36 -0500

    Update FDR-GPWAS.R

--- utilis/FDR-GPWAS.R
+++ utilis/FDR-GPWAS.R
@@ -3,12 +3,11 @@ ori = read.table(args[1],sep=' ',head=F) # P-value generated from original pheno
 perm = read.table(args[2],sep=' ',head=F) # P-value generated from permuted phenotype data in GPWAS
 ori_p = sort(ori$V3)
 perm_p = sort(perm$V3)
-start = 0.1 # start of p-value threshold
 loop_number = 30 # picking a loop number for iterating p-value threshold
 step = 2 # picking any integer number, the smaller value the narrower range of p-value threshold you can choose
 print ("P-value threshold; FDR; Filtered Gene Number")
-for (i in c(1:30)){
-  thres = step**(-1*i)
+for (i in c(1:loop_number)){
+  thres = step**(-1*i) # iterating different p-value threshold 
   ori_count = 0
   perm_count = 0
   for (o in ori_p){
